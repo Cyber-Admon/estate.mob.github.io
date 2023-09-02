fff1d1103e08b177901edb650eaec7c75dc40a55
MOB Technologies <142998415+MOB-Technologies@users.noreply.github.com>
2023-09-02 05:39:03 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import Nav from './assets/images/components/Nav'
+
+function App() {
+ return (
+ <Nav />
+ )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav(){
+    <div>
+    <h1> navBAr</h1>
+    </div>
+}
+
+export default Nav;
